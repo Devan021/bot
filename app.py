2d0933aa7a52cb4842c2ff4bf96bd8f46460b6e8
Devan021 <devansp2020@gmail.com>
2024-10-27 18:10:47 +0530

    commit

--- app.py
+++ app.py
@@ -18,5 +18,16 @@ def whatsapp():
     msg.body(response_text)
     return str(resp)
 
+@app.route("/status", methods=['POST','GET'])
+def status():
+    if request.method=='GET':
+        return "Status callback received via GET", 200
+    elif request.method=='POST':
+        return "Status callback recieved via POST", 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=(os.environ.get("PORT",5000)))
+
+
+
